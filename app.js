d1df70acb9342946fc1732b595eeac3b5c2174bd
DOTG29 <dgoris@edenschool.fr>
2023-04-27 11:42:02 +0200

    algo finit

---
+++ app.js
@@ -0,0 +1,39 @@
+// let tab = [12,15,48,78,3]
+
+// for (let i = 1; i < tab.length; i++) {
+//     let x = tab[i];
+//     let j = i;
+//     while (j > 0 && tab[j - 1] > x) {
+//         tab[j] = tab[j - 1];
+//         j = j - 1
+//     }    
+//     tab[j] = x
+    
+// }
+
+// console.log(tab);
+
+
+
+// export function trie(tab) {
+//     for (let i = 1; i < tab.length; i++) {
+//         let x = tab[i];
+//         let j = i;
+//         while (j > 0 && tab[j - 1] > x) {
+//             tab[j] = tab[j - 1];
+//             j = j - 1
+//         }
+//         tab[j] = x
+        
+//     }
+
+//     return tab
+// }
+
+// let arr = [[1,2,3],
+//            [12,15,40],
+//            [12,16,50],
+//            [12,13,20],
+//            [50,42,21]];
+
+// console.log(trie(arr));
